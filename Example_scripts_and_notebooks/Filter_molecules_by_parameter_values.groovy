2b7d744ee002fa374f2e6c339590955019f2ef9f
Nadia <huisjes@biochem.mpg.de>
2020-06-02 14:39:20 +0200

    fix script

--- Example_scripts_and_notebooks/Filter_molecules_by_parameter_values.groovy
+++ Example_scripts_and_notebooks/Filter_molecules_by_parameter_values.groovy
@@ -5,7 +5,7 @@
 import de.mpg.biochem.mars.molecule.*
 import de.mpg.biochem.mars.table.*
 
-archive.stream().filter{ molecule -> molecule.getParameter("reversal") < 0.5}.forEach{ molecule ->
+archive.molecules().filter{ molecule -> molecule.getParameter("reversal") < 0.5}.forEach{ molecule ->
    //This would tag all molecule with reversal below 0.5
    molecule.addTag("background")
    archive.put(molecule);
