56e682828645fd5a59547528c413065b10e05fea
Nadia <huisjes@biochem.mpg.de>
2020-09-07 16:08:42 +0200

    debugged

--- Example_scripts_and_notebooks/Add_flowrates_from_metadata_to_molecule_records.groovy
+++ Example_scripts_and_notebooks/Add_flowrates_from_metadata_to_molecule_records.groovy
@@ -1,4 +1,5 @@
 //Adds flowrates from metadata to molecule records
+//Run 'Add flowrate (ul/min) to the metadata' first to add flowrates to the metadata of the Archive
 
 #@ MoleculeArchive archive
 
@@ -7,101 +8,17 @@ import de.mpg.biochem.mars.molecule.*
 import de.mpg.biochem.mars.table.*
 import org.scijava.table.*
 
-archive.lock()
 
 MarsMetadata metadata = archive.getMetadata(0)
 
 archive.molecules().forEach{ molecule ->
   MarsTable table = molecule.getDataTable()
+
   if (!table.hasColumn("Flowrate (ul/min)"))
     table.appendColumn("Flowrate (ul/min)")
 
   for (int row=0;row<table.getRowCount();row++){
+    int T = (int) table.getValue("T", row)
     table.set("Flowrate (ul/min)", row,metadata.getImage(0).getPlane(0, 0, T).getField("Flowrate (ul/min)") )
-}}
----
-// Add flowrates from a T,flowrate table to individual molecule records
-#@ MarsTable tToFlowTable
-#@ MoleculeArchive archive
-
-import de.mpg.biochem.mars.metadata.*
-import de.mpg.biochem.mars.molecule.*
-import de.mpg.biochem.mars.table.*
-import org.scijava.table.*
-import java.util.HashMap;
-
-//First lets build a map from slice to flowrate
-tToFlowMap = [:]
-
-for (int row=0;row<tToFlowTable.getRowCount();row++)
-   tToFlowMap.put((int) tToFlowTable.getValue("T", row), tToFlowTable.getValue("flowrate", row))
-
-MarsMetadata metadata = archive.getMetadata(0)
-
-
-
-archive.molecules().forEach{ molecule ->
-  MarsTable table = molecule.getDataTable()
-  if (!table.hasColumn("Flowrate (ul/min)"))
-    table.appendColumn("Flowrate (ul/min)")
-
-  for (int row=0;row<table.getRowCount();row++){
-    table.set("Flowrate (ul/min)", row,metadata.getImage(0).getPlane(0, 0, row).getField("Flowrate (ul/min)") )
-}}
-
-
-
-archive.putMetadata(metadata)
-
-
-
----
-//First make key value pairs of T values and flow rates
-TToFlowrate = [:]
-
-
-TToFlowrate.put((int) 1.0, 2.0)
-TToFlowrate.put((int) 1.0, metadata.getImage(0).getPlane(0, 0, 1).getField("Flowrate (ul/min)"))
-
-println(TToFlowrate)
-
-
-for (int row=0;metadata.getImage(0).getSizeT();row++){
-  TToFlowrate.put(row, metadata.getImage(0).getPlane(0, 0, row).getField("Flowrate (ul/min)") )
+  }
 }
-
-println(TToFlowrate)
-
-
-
-println(metadata.getImage(0).getPlane(0, 0, 1).getField("Flowrate (ul/min)"))
-println(metadata.getImage(0).getSizeT())
-
-
--------
-
-sliceToFlowrate = [:]
-
-for (int row=0;row<metaTable.getRowCount();row++) {
-   sliceToFlowrate.put(metaTable.getValue("slice", row), metaTable.getValue("Flowrate (ul/min)", row))
-}
-
-println(sliceToFlowrate)
-
-archive.getMoleculeUIDs().stream().forEach{ UID ->
-   molecule = archive.get(UID)
-   table = molecule.getDataTable()
-
-   //If the column already exists we don't need to add it
-   //instead we will just be overwriting the values below..
-   if (!table.hasColumn("Flowrate (ul/min)"))
-      table.appendColumn("Flowrate (ul/min)")
-
-   for (int row=0;row<table.getRowCount();row++) {
-      table.set("Flowrate (ul/min)", row, sliceToFlowrate.get(table.get("slice", row)))
-   }
-
-   archive.put(molecule)
-}
-
-archive.unlock()
--- Example_scripts_and_notebooks/Add_flowrates_to_metadata.groovy
+++ Example_scripts_and_notebooks/Add_flowrates_to_metadata.groovy
@@ -1,4 +1,4 @@
-//Adds a flowrate (ul/min) column to the metadata record.
+//Add flowrate (ul/min) to the metadata
 
 #@ MarsTable tToFlowTable
 #@ MoleculeArchive archive
@@ -19,14 +19,3 @@ MarsMetadata metadata = archive.getMetadata(0)
 
 for (int indexT=0; indexT<metadata.getImage(0).getSizeT(); indexT++)
      metadata.getImage(0).getPlane(0, 0, indexT).setField("Flowrate (ul/min)", tToFlowMap.get(indexT))
-
-	//setField(String key, double value) There is also
-	//setStringField(String key, String value)...
-
-     //FYI - Same thing
-     //metadata.getPlane(0, 0, 0, t).setField(sliceToFlowMap.get(t))
-     //This getPlane method directly on the MarsMetadata object takes the image index as the first input.
-     //Inside it does the same as above... Maybe I should remove it?..
-
-
-archive.putMetadata(metadata)
